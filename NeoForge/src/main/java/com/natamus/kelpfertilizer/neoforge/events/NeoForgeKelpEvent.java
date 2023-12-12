e1775986b1af466a98adecd172eb9e25b518c26f
Rick South <rick@natamus.com>
2023-12-12 09:45:48 +0100

    Kelp Fertilizer source code for 1.20.4.

---
+++ NeoForge/src/main/java/com/natamus/kelpfertilizer/neoforge/events/NeoForgeKelpEvent.java
@@ -0,0 +1,14 @@
+package com.natamus.kelpfertilizer.neoforge.events;
+
+import com.natamus.kelpfertilizer.events.KelpEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeKelpEvent {
+	@SubscribeEvent
+	public static void onKelpUse(PlayerInteractEvent.RightClickBlock e) {
+		KelpEvent.onKelpUse(e.getLevel(), e.getEntity(), e.getHand(), e.getPos(), e.getHitVec());
+	}
+}
