406567ca1a3a58d0ce770e74bccfda0ea1d6b75a
Rick South <rick@natamus.com>
2024-05-01 16:36:59 +0200

    Kelp Fertilizer source code for 1.20.6.

---
+++ Common/src/main/java/com/natamus/kelpfertilizer/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.kelpfertilizer.util;
+
+public class Reference {
+	public static final String MOD_ID = "kelpfertilizer";
+	public static final String NAME = "Kelp Fertilizer";
+	public static final String VERSION = "3.2";
+	public static final String ACCEPTED_VERSIONS = "[1.20.6]";
+}
