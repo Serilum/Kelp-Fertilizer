64f6b1105861e5206a9e55b2cfa425b3ea7fb335
Rick South <rick@natamus.com>
2024-07-28 07:26:27 +0200

    (v3.3) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Fabric/src/main/java/com/natamus/kelpfertilizer/ModFabric.java
+++ Fabric/src/main/java/com/natamus/kelpfertilizer/ModFabric.java
@@ -1,6 +1,7 @@
 package com.natamus.kelpfertilizer;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.collective.fabric.callbacks.CollectiveBlockEvents;
 import com.natamus.kelpfertilizer.events.KelpEvent;
 import com.natamus.kelpfertilizer.util.Reference;
@@ -15,6 +16,10 @@ public class ModFabric implements ModInitializer {
 	
 	@Override
 	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		setGlobalConstants();
 		ModCommon.init();
 
