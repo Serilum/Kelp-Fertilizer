18e7143f43ae87bd35a972f3c2779bd85edad6c7
Rick South <rick@natamus.com>
2025-02-12 23:26:52 +0100

    (v3.4) (Forge & Neo) Removed redundant event register annotations. [+1]

--- NeoForge/src/main/java/com/natamus/kelpfertilizer/neoforge/events/NeoForgeKelpEvent.java
+++ NeoForge/src/main/java/com/natamus/kelpfertilizer/neoforge/events/NeoForgeKelpEvent.java
@@ -3,9 +3,7 @@ package com.natamus.kelpfertilizer.neoforge.events;
 import com.natamus.kelpfertilizer.events.KelpEvent;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 
-@EventBusSubscriber
 public class NeoForgeKelpEvent {
 	@SubscribeEvent
 	public static void onKelpUse(PlayerInteractEvent.RightClickBlock e) {
