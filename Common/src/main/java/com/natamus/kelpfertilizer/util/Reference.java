eee5df6e0bf92f7a45127d0d11e688f7911f4ceb
Rick South <rick@natamus.com>
2026-06-18 23:42:31 +0200

    Updated Kelp Fertilizer to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/kelpfertilizer/util/Reference.java
+++ Common/src/main/java/com/natamus/kelpfertilizer/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "kelpfertilizer";
 	public static final String NAME = "Kelp Fertilizer";
 	public static final String VERSION = "3.4";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
